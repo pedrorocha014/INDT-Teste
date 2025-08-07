59b5227efbcc62151740a4e1114737ae27f32104
Pedro Almeida Rocha <19pedrorocha96@gmail.com>
2025-08-07 15:37:30 -0300

    fix retorno com enum value

--- src/PropostaService/Program.cs
+++ src/PropostaService/Program.cs
@@ -1,5 +1,6 @@
 using Infrastructure;
 using Applications;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +9,11 @@ builder.Services.AddApplicationsServices(builder.Configuration);
 
 builder.Services.AddHttpClient();
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(opt =>
+    {
+        opt.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    });
 
 builder.Services.Configure<RouteOptions>(options =>
 {
